9b35bb119eadaa6381aba0a60be8e7e3067d8d60
lzt <1071453851@qq.com>
2019-09-23 15:59:17 +0800

    ADD USER_AGENT

--- src/main/java/http/HttpConstant.java
+++ src/main/java/http/HttpConstant.java
@@ -10,7 +10,7 @@ public class HttpConstant {
     public static final String MEDIA_TYPE_NORMAL = "application/x-www-form-urlencoded";// 数据是个普通表单
 
     /**
-     * file
+     * 文件格式
      */
     public static final String FILE_MIXED = "multipart/mixed";
     public static final String FILE_ALTERNATIVE = "multipart/alternative";
@@ -18,9 +18,19 @@ public class HttpConstant {
     public static final String FILE_PARALLEL = "multipart/parallel";
     public static final String FILE_FORM = "multipart/form-data";
 
+    /**
+     * 时间
+     */
     public static final Integer CONNECT_TIME_OUT = 60 * 1000;
     public static final Integer READ_TIME_OUT = 5 * 60 * 1000;
     public static final Integer WRITE_TIME_OUT = 5 * 60 * 1000;
+
+    /**
+     * User-Agent
+     * 模拟浏览器登录
+     */
+    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36";
+
 
     public enum HttpMethod{
         GET,POST,DELETE,PUT,FORM_DATA,POST_FORM;
